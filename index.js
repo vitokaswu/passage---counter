6c60f25dfd13b46956e21aa340b00e922b26e59b
vitokaswu <126566160+vitokaswu@users.noreply.github.com>
2023-11-14 22:05:01 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let count = 0
+
+let countEl = document.getElementById("count-el")
+
+let saveEl = document.getElementById("save-el")
+
+console.log(countEl)
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    }   
+
+
